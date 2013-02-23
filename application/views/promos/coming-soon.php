759331fd0ab93a6edbb46a47052eb66e4cb5ced2
Michael Wales <webmaster@michaelwales.com>
2013-02-23 07:43:37 +0000

    Coming Soon page designed

---
+++ application/views/promos/coming-soon.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <title>San Antonio House &amp; Home Cleaning | Danielle Mendes, Epic Maid</title>
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width" />       
+
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.0/css/bootstrap-combined.no-icons.min.css" />
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css" />
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css" />
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700" />
+
+    <script src="<?php echo base_url(); ?>assets/js/html5shiv-printshiv.js"></script>
+</head>
+<body class="coming-soon">
+    <div class="container content">
+        <p class="pull-right"><i class="icon-phone"></i> 210.453.1298</p>
+
+        <div class="row-fluid">
+            <header class="span12">
+                <h1><span>epic</span>maid</h1>
+                <p>Danielle Mendes | epicmaid.com</p>
+            </header>
+        </div>
+
+        <div class="row-fluid content-body">
+            <div class="span6">
+                <img src="<?php echo base_url(); ?>assets/imgs/housekeeper.png" alt="Danielle Mendes, Epic Maid" title="Danielle Mendes, Epic Maid" />
+            </div>
+
+            <div class="span6">
+                <h3>About Danielle</h3>
+                <p>For 6 years I have provided affordable cleaning for the best clients in San Antonio. 
+                Before that? <strong>US Marine.</strong> Cleaning up someone else's mess is pretty much my thing.</p>
+
+                <p>Imagine what you could do with just a few extra hours per week! A long overdue date night or maybe ice 
+                    cream cones after baseball practice? Then come home to freshly vacuumed carpets, mopped floors, and not
+                    a dirty dish in sight.</p>
+
+
+                <div class="next-neighborhood"><p><span class="label label-info">Alamo Ranch</span> 
+                    will start booking soon and will <em>go fast!</em> If I can get in touch with you, I'll make sure you're sent a 
+                    message the instant I start scheduling.</p>
+
+                    <form class="form-inline">
+                        <div class="input-prepend">
+                            <span class="add-on"><i class="icon-user"></i></span>
+                            <input type="text" id="name" class="input" placeholder="Your name" name="name" required />
+                        </div>
+                        <div class="input-prepend">
+                            <span class="add-on"><i class="icon-envelope"></i></span>
+                            <input type="email" id="email_address" class="input" placeholder="Your email address" name="email_address" required />
+                        </div>
+                        <input type="submit" class="btn-small btn-primary" value="Save" disabled />
+                    </form>
+                </div>
+
+                <img src="<?php echo base_url(); ?>assets/imgs/alamoranch.png" alt="Northwest San Antonio: Alamo Ranch" title="Northwest San Antonio: Alamo Ranch" class="img-polaroid" />
+            </div>
+        </div>
+    </div>
+
+    <script src="//code.jquery.com/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="<?php echo base_url(); ?>assets/js/jquery-1.9.1.min.js"><\/script>')</script>
+</body>
+</html>
