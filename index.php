f6ab6c4013fb52417403a7698cbc542f3e9d47eb
Michael Wales <webmaster@michaelwales.com>
2013-05-04 13:59:01 -0500

    Silex now processes requests

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
-<?php require_once __DIR__ . 'vendor/autoload.php';
-
+<?php require_once __DIR__ . '/vendor/autoload.php';
 $app = new Silex\Application();
 
+
+$app['debug'] = true;
+
 // Controllers & Routes
+$app->get('/', function() {
+    return 'Yay';
+});
+
 
 $app->run();
